4fc8d4e6956fb6459f87af219a3acb561844d21f
苏日甫 <1020343499@qq.com>
2016-06-20 11:39:15 +0800

    输出0加到100的值
    用var定义一个变量sum用来储存相加结果，定义i用来计数。利用for循环遍历100次，每遍历一次都将sum与i的值相加，最后输出sum的值。

---
+++ main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  3
+//
+//  Created by 20141105039 on 16/6/20.
+//  Copyright © 2016年 20141105039. All rights reserved.
+//
+
+import Foundation
+var sum = 0
+var i = 0
+for number in 0...100{
+    sum = sum + i
+    i++
+}
+print("\(sum)")
+
